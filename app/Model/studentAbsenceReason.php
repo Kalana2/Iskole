e8a2dd3041a17ddad8e26f66796605240461b762
Kalana <kalanajinendra@gmail.com>
2025-11-23 01:31:59 +0530

    student absence reason create model

--- app/Model/studentAbsenceReason.php
+++ app/Model/studentAbsenceReason.php
@@ -2,9 +2,33 @@
 
 class StudentAbsenceReason
 {
-
+    protected $pdo;
     private $table = "absentReasons";
 
+    public function __construct()
+    {
+        $this->pdo = Database::getInstance();
+    }
+    public function submitAbsenceReason($data)
+    {
+        try {
+            $stmt = $this->pdo->prepare(
+                "INSERT INTO " . $this->table . " (parentID, studentID, teacherID, reason, fromDate, toDate) 
+                 VALUES (:parentId, :studentId, :teacherId, :reason, :fromDate, :toDate)"
+            );
+            return $stmt->execute([
+                'parentId' => $data['parentId'],
+                'studentId' => $data['studentId'],
+                'teacherId' => $data['teacherId'],
+                'reason' => $data['reason'],
+                'fromDate' => $data['fromDate'],
+                'toDate' => $data['toDate'],
+            ]);
+        } catch (PDOException $e) {
+            error_log('Failed to submit absence reason: ' . $e->getMessage());
+            return false;
+        }
+    }
 
 
 }
