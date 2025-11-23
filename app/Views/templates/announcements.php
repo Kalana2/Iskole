d2b71e5923a480b90bd0ea13215dfb34cc791dbe
Seniru Seneweera <152300004+senirudevidu@users.noreply.github.com>
2025-11-23 09:06:36 +0530

    Delete button added ti announcement ui

--- app/Views/templates/announcements.php
+++ app/Views/templates/announcements.php
@@ -60,6 +60,19 @@
                             </svg>
                             Edit
                         </button>
+                        <button class="btn-delete" type="button"
+                            data-id="<?php echo htmlspecialchars($a['id'] ?? $i); ?>"
+                            data-title="<?php echo htmlspecialchars($a['title'] ?? '', ENT_QUOTES); ?>"
+                            data-body="<?php echo htmlspecialchars($a['body'] ?? '', ENT_QUOTES); ?>"
+                            data-audience="<?php echo htmlspecialchars($a['audience'] ?? 'all', ENT_QUOTES); ?>">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash-icon lucide-trash">
+                                <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+                                <path d="M3 6h18" />
+                                <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                            </svg>
+                            Delete
+                        </button>
+
                     </div>
                 <?php endif; ?>
             </article>
