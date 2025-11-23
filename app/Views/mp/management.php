f3e9c106289aa95ffcc6488fa3b695372abc05ce
Kalana <kalanajinendra@gmail.com>
2025-11-23 18:54:56 +0530

    addNewUser completed

--- app/Views/mp/management.php
+++ app/Views/mp/management.php
@@ -1,16 +1,19 @@
 <section class="management">
-    <?php if (session_status() === PHP_SESSION_NONE) {
-        session_start();
-    } ?>
-    <?php if (!empty($_SESSION['mgmt_msg'])): ?>
-        <div class="alert alert-info" role="status"><?php echo htmlspecialchars($_SESSION['mgmt_msg']); ?></div>
-        <?php unset($_SESSION['mgmt_msg']); ?>
-    <?php endif; ?>
-
-    <?php if (!empty($_GET['debug']) && $_GET['debug'] === '1'): ?>
-        <pre class="debug-session"><?php echo htmlspecialchars(print_r($_SESSION, true)); ?></pre>
-    <?php endif; ?>
-
+    <!-- deguggin -->
+    <?php
+    // if (session_status() === PHP_SESSION_NONE) {
+    //     session_start();
+    // }
+    
+    // if (!empty($_SESSION['mgmt_msg'])) {
+    //     echo '<div class="alert alert-info" role="status">' . htmlspecialchars($_SESSION['mgmt_msg']) . '</div>';
+    //     unset($_SESSION['mgmt_msg']);
+    // }
+    
+    // if (!empty($_GET['debug']) && $_GET['debug'] === '1') {
+    //     echo '<pre class="debug-session">' . htmlspecialchars(print_r($_SESSION, true)) . '</pre>';
+    // }
+    ?>
     <?php include_once __DIR__ . '/../templates/userDirectory.php'; ?>
     <?php include_once __DIR__ . '/../templates/addNewUser.php'; ?>
 </section>
