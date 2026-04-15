b02f7f3499729d25e245867c73d04c82b5468fb2
Kalana <kalanajinendra@gmail.com>
2026-04-15 13:11:10 +0530

    search by role

--- app/Controllers/userDirectoryController.php
+++ app/Controllers/userDirectoryController.php
@@ -40,7 +40,7 @@ class UserDirectoryController extends Controller
         if ($query) {
             // Convert role name (Admin) → role ID (0)
             $roleKey = array_search($query, $this->userRoleMap);
-            // var_dump("roleKey");
+            var_dump("roleKey");
             // var_dump($roleKey);
             if ($roleKey !== false) {
                 $query = $roleKey;
